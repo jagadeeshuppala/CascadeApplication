db47cdb78d65c48f5d7bb993fbf22f4e46740d12
Jagadeesh <uppala.jagadeesh@gmail.com>
2025-07-18 19:13:47 +0100

    from pharmacy desktop

--- src/main/java/com/pharmacy/bridgwater/CascadeApp/Desktop.java
+++ src/main/java/com/pharmacy/bridgwater/CascadeApp/Desktop.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
 import static com.pharmacy.bridgwater.CascadeApp.constants.Constants.*;
 
 
-public class Local {
+public class Desktop {
 
         //In Pharmacy
 
